3d69c23d5997403422369dff96b74818088dd043
Gennaro Anzalone and Rafael Furry <rafael@>
2017-08-22 14:39:31 -0700

    init the bank account webpage

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#submit').click(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var initDeposit = $("#initDeposit").val();
+    var deposit = $("#deposit").val();
+    var withdrawal = $("#withdrawal").val();
+
+    
+
+
+
+
+
+  });
+});
